f618c9370c912bc9b8bc9393d381a33f0eb7e3e4
Thomas Rothe <thomas.rothe@little-bird.de>
2013-01-23 13:03:57 +0100

    fixed composer.json and test autoloader to work with 2.4
    Conflicts:
    	tests/Doctrine/Tests/TestInit.php

--- tests/Doctrine/Tests/TestInit.php
+++ tests/Doctrine/Tests/TestInit.php
@@ -5,21 +5,30 @@
 namespace Doctrine\Tests;
 
 error_reporting(E_ALL | E_STRICT);
-
+defined('VENDOR_DIR')?:define('VENDOR_DIR',__DIR__ . '/../../../vendor');
+require_once VENDOR_DIR.'/doctrine/common/lib/Doctrine/Common/ClassLoader.php';
 
 if (isset($GLOBALS['DOCTRINE_COMMON_PATH'])) {
-    require_once $GLOBALS['DOCTRINE_COMMON_PATH'] . '/Doctrine/Common/ClassLoader.php';
     $classLoader = new \Doctrine\Common\ClassLoader('Doctrine\Common', $GLOBALS['DOCTRINE_COMMON_PATH']);
 } else {
-    require_once __DIR__ . '/../../../lib/vendor/doctrine-common/lib/Doctrine/Common/ClassLoader.php';
-    $classLoader = new \Doctrine\Common\ClassLoader('Doctrine\Common', __DIR__ . '/../../../lib/vendor/doctrine-common/lib');
+    $classLoader = new \Doctrine\Common\ClassLoader('Doctrine\Common\Collections', VENDOR_DIR . '/doctrine/collections/lib');
+    $classLoader->register();
+    $classLoader = new \Doctrine\Common\ClassLoader('Doctrine\Common\Annotations', VENDOR_DIR . '/doctrine/annotations/lib');
+    $classLoader->register();
+    $classLoader = new \Doctrine\Common\ClassLoader('Doctrine\Common\Cache', VENDOR_DIR . '/doctrine/cache/lib');
+    $classLoader->register();
+    $classLoader = new \Doctrine\Common\ClassLoader('Doctrine\Common\Inflector', VENDOR_DIR . '/doctrine/inflector/lib');
+    $classLoader->register();
+    $classLoader = new \Doctrine\Common\ClassLoader('Doctrine\Common\Lexer', VENDOR_DIR . '/doctrine/lexer/lib');
+    $classLoader->register();
+    $classLoader = new \Doctrine\Common\ClassLoader('Doctrine\Common', VENDOR_DIR . '/doctrine/common/lib');
 }
 $classLoader->register();
 
 if (isset($GLOBALS['DOCTRINE_DBAL_PATH'])) {
     $classLoader = new \Doctrine\Common\ClassLoader('Doctrine\DBAL', $GLOBALS['DOCTRINE_DBAL_PATH']);
 } else {
-    $classLoader = new \Doctrine\Common\ClassLoader('Doctrine\DBAL', __DIR__ . '/../../../lib/vendor/doctrine-dbal/lib');
+    $classLoader = new \Doctrine\Common\ClassLoader('Doctrine\DBAL', VENDOR_DIR.'/doctrine/dbal/lib');
 }
 $classLoader->register();
 
@@ -29,16 +38,19 @@ $classLoader->register();
 $classLoader = new \Doctrine\Common\ClassLoader('Doctrine\Tests', __DIR__ . '/../../');
 $classLoader->register();
 
-$classLoader = new \Doctrine\Common\ClassLoader('Symfony', __DIR__ . "/../../../lib/vendor");
+$classLoader = new \Doctrine\Common\ClassLoader('Symfony\Component\Yaml', VENDOR_DIR.'/symfony/yaml');
+$classLoader->register();
+
+$classLoader = new \Doctrine\Common\ClassLoader('Symfony\Component\Console', VENDOR_DIR.'/symfony/console');
 $classLoader->register();
 
 if (!file_exists(__DIR__."/Proxies")) {
     if (!mkdir(__DIR__."/Proxies")) {
-        throw new Exception("Could not create " . __DIR__."/Proxies Folder.");
+        throw new \Exception("Could not create " . __DIR__."/Proxies Folder.");
     }
 }
 if (!file_exists(__DIR__."/ORM/Proxy/generated")) {
     if (!mkdir(__DIR__."/ORM/Proxy/generated")) {
-        throw new Exception("Could not create " . __DIR__."/ORM/Proxy/generated Folder.");
+        throw new \Exception("Could not create " . __DIR__."/ORM/Proxy/generated Folder.");
     }
 }
